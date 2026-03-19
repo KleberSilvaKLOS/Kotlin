68e53d2507a6cf1a27e643740f8ceb50e06f86d7
KleberSilvaKLOS <kleluca75@gmail.com>
2026-03-19 17:35:41 -0300

    Finalizamos a atividade, agora vamos rodar ele em jar e verificar se existe algum erro no processo.

--- Classes Singletons/sistemaseguranca.kt
+++ Classes Singletons/sistemaseguranca.kt
@@ -48,3 +48,37 @@ object GerenciadorSessão {
     }
 }
 
+fun main() {
+    // Tentando ação sem estar logado
+    GerenciadorSessao.realizarAcao("Acessar Banco de Dados") // Deve mostrar erro
+
+    // Criando usuário com Data Classes
+    val u1 = Usuario(1, "Alice", "Admin")
+    val u2 = Usuario(2, "Bob", "Editor")
+
+    // Realizando login com o singleton
+    println("\n--- Iniciando Sessão ---")
+    gerenciadorSessao.login(u1) // Alice loga
+
+    // Realizando ações
+    GerenciadorSessao.realizarAcao("Atualizar Configurações") // Deve ser permitido
+    GerenciadorSessao.realizarAcao("Exportar Relatório Mensal") // Deve ser permitido
+
+    // Trocando de usuário (Simulando persistência do Singleton)
+    GerenciadorSessao.logout()
+    GerenciadorSessao.login(u2) // Bob loga
+    GerenciadorSessao.realizarAcao("Editar Perfil") // Deve ser permitido
+
+    // Verificando todos os logs acumulados no Singleton
+    GerenciadorSessao.imprimirRelatorio()
+}
+
+// O QUE ESTAMOS APRENDENDO AQUI?
+// 1. Estado Persistente: Note que o "GerenciadorSessão" guarda o "historicoAcoes". 
+// Mesmo que você mude de usuário ou chame funções diferentes, os logs antigos continuam lá porque o objeto é único 
+
+// 2. Encapsulamento: O "historicoAcoes" é privado. 
+// Ninguém no "main" consegue apagar os logs, apenas o próprio Singleton pode adicionar mensagens via "registrarAcao". 
+// Isso é um exemplo de encapsulamento, onde controlamos como os dados são acessados e modificados.
+
+// 3. Flexibilidade: Usamos as propriedades do "Usuario" de forma fácil dentro das strings do Singleton.
